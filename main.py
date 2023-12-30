13e831e3a21d672b43e8b05b97e50bdb7b67e8a6
rithwickcenddle <rithwick@cenddle.com>
2023-12-30 12:21:39 +0530

    requirement.txt new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import mean_var_std
 from unittest import main
 
-print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+print(mean_var_std.calculate([2,6,2,8,4,0,1,5,7]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
